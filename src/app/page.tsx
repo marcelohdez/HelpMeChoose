e363c5ac35c85d8784bec0f516d75177523826ec
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2026-02-16 19:47:18 -0600

    new: add/remove columns and attributes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
+"use client";
+
 import { Column } from "@/components/Column";
+import { useState } from "react";
 
 const Home = () => {
-  const choices = ["one", "two", "three", "four", "five"];
+  const [columns, setColumns] = useState<number[]>([]);
+
+  const add = () =>
+    columns.length < 5
+      ? setColumns(columns.concat(columns.length + 1))
+      : console.log("Maximum number of columns reacehed.");
 
   return (
-    <div className="flex items-center m-2 sm:m-4 mt-0 flex-col gap-2 sm:gap-4">
-      <div className="flex w-full max-w-6xl justify-center-safe overflow-scroll gap-2 sm:gap-4">
-        {choices.map((x, i) => (
-          <Column key={i} title={x} />
+    <div className="flex items-center flex-col gap-2 sm:gap-4">
+      <button
+        className="rounded-md py-1 px-2 opacity-50 hover:opacity-100 hover:bg-neutral-500/10"
+        onClick={() => add()}
+      >
+        + Column
+      </button>
+      <div
+        className="flex w-full max-w-7xl justify-center-safe overflow-scroll px-2 sm:px-4
+        gap-2 sm:gap-4"
+      >
+        {columns.map((x, i) => (
+          <Column key={i} title={x.toString()} />
         ))}
       </div>
       <div>Decision: one</div>
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,30 +1,51 @@
+"use client";
+
+import { useState } from "react";
+
 interface ColumnProps {
   title: string;
 }
 
 const Column = (props: ColumnProps) => {
-  const attributes = ["one", "two", "three"];
+  const [attributes, setAttributes] = useState<number[]>([]);
+
+  const remove = (idx: number) =>
+    setAttributes(attributes.filter((_, i) => i != idx));
+
+  const add = () => setAttributes(attributes.concat(attributes.length + 1));
 
   return (
-    <div
-      className="grow rounded-lg gap-2 max-w-md min-w-40 sm:min-w-48
-      bg-neutral-100 dark:bg-neutral-800 p-3 px-4 sm:p-4 sm:px-6 border
-      border-neutral-300 dark:border-neutral-700 drop-shadow-md "
-    >
-      <div className="mb-1">I am option {props.title}</div>
-      <ul>
-        {attributes.map((x, i) => (
-          <div
-            key={i}
-            className="mb-2 rounded-md py-1 px-2 sm:py-2 sm:px-4 bg-neutral-200 dark:bg-neutral-700"
-          >
-            {x}
-          </div>
-        ))}
-      </ul>
-      <button className="opacity-50 hover:opacity-100 hover:px-2">
-        + Attribute
-      </button>
+    <div className="grow max-w-sm min-w-40 sm:min-w-48 md:min-w-52">
+      <div
+        className="flex flex-col rounded-lg gap-2 bg-neutral-100 dark:bg-neutral-800 p-2 sm:px-4
+        border-2 border-neutral-300/50 dark:border-neutral-700/50 shadow-md"
+      >
+        <div className="text-center sm:p-1">{props.title}</div>
+        <ul className="flex flex-col gap-2">
+          {attributes.map((x, i) => (
+            <li
+              key={i}
+              className="flex justify-between gap-2 rounded-md py-1 px-2 md:py-2 md:px-4
+            bg-neutral-200 dark:bg-neutral-700/50 shadow-md group"
+            >
+              {x}
+              <button
+                className="opacity-0 group-hover:opacity-100"
+                onClick={() => remove(i)}
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button
+          className="text-left opacity-50 rounded-md py-1 px-2 hover:bg-neutral-500/10
+          hover:opacity-100"
+          onClick={() => add()}
+        >
+          + Attribute
+        </button>
+      </div>
     </div>
   );
 };
